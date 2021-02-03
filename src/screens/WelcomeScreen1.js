06c5732e8b5de7f8143c13d8bb70f276e7d080b5
justice sowah <justice.sowah18@gmail.com>
2021-02-03 20:45:59 +0000

    welcome screen one modified by removing margin styling and using flex

--- src/screens/WelcomeScreen1.js
+++ src/screens/WelcomeScreen1.js
@@ -5,15 +5,15 @@ import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native'
 export default function WelcomeScreen1({navigation}) {
     return (
         <View style={styles.container}>
-            <View style={{marginTop: 150, flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
+            <View style={{flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
                 <Image source={require('../../assets/amp_icon_01.png')} />
-                <Text style={{marginTop: 18, fontSize: 25, fontWeight: "bold", marginLeft: -2, color: "#434343"}}>AMPERSAND</Text>
+                <Text style={{fontSize: 25, fontWeight: "bold", marginLeft: -2, color: "#434343"}}>AMPERSAND</Text>
             </View>
-            <View style={{justifyContent: "center", alignItems: "center", marginBottom: -300, marginTop: 150}}>
+            <View style={{justifyContent: "center", alignItems: "center"}}>
                 <Text style={{color: "gray", fontSize: 25}}>AMPERSAND</Text>
                 <Text style={{color: "gray", fontSize: 25}}>CONTACTS</Text>
             </View>
-            <View style={{marginTop: 500, justifyContent: "center", alignItems:"center"}}>
+            <View style={{justifyContent: "center", alignItems:"center"}}>
                 <TouchableOpacity onPress={()=>{navigation.navigate("RegisterLogin")}}>
                     <Text style={{color: "gray", fontSize: 20}}>GET STARTED</Text>
                     <View style={{height: 2, width: 127, backgroundColor: "orange"}}></View>
@@ -25,6 +25,7 @@ export default function WelcomeScreen1({navigation}) {
 
 const styles=StyleSheet.create({
     container: {
+        justifyContent: 'space-around',
         backgroundColor: "#EAEBDC",
         height: 770,
         width: 360
